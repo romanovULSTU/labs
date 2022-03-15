c3bfd14381ad36910ed754320aa2ebab62b5a985
romanovULSTU <99944605+romanovULSTU@users.noreply.github.com>
2022-03-15 20:20:21 +0400

    Update 2lab.py

--- 2lab.py
+++ 2lab.py
@@ -1,17 +1,21 @@
-data = open ("input_for_2lab.txt")
-n_list = list(data)
-print('Числа с  обратным порядком цифр:\n')
-for n in n_list:
-    n = int(n)
-    if n >10000:
-        print(n%10,(n//10)%10,(n//100)%10,(n//1000)%10,(n//10000)%10,sep='')
-    elif 1000< n <10000:
-        print(n%10,(n//10)%10,(n//100)%10,(n//1000)%10,sep='')
-    elif 100< n <1000:
-        print(n%10,(n//10)%10,(n//100)%10,sep='')
-    elif  10< n <100:
-        print(n%10,(n//10)%10,sep='')
-    else:
-        print(n)
-        
+import cProfile, pstats  ,io    #4052 function calls in 0.091 seconds
+from pstats import SortKey               
+pr = cProfile.Profile()                         
+pr.enable()
+
+ 
+data = open ("input.txt",'r')   #открываем файл                 
+while True:
+    line = data.readline()                                                                                 
+    if not line:                #выходим из цикла , если строка пустая     
+        break                    
+    print(line.strip()[::-1])   #выводим переворот срезом
+
+    
+pr.disable()
+s = io.StringIO()
+sortby = SortKey.CUMULATIVE
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
 
