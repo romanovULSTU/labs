a7b8d89a3799d6500a42d651d21e7d052ecbc6a4
romanovULSTU <99944605+romanovULSTU@users.noreply.github.com>
2022-03-15 20:28:42 +0400

    Update 2lab_alternative_solution.py

--- 2lab_alternative_solution.py
+++ 2lab_alternative_solution.py
@@ -1,18 +1,4 @@
-import cProfile, pstats, io     #4014 function calls in 0.107 seconds
-from pstats import SortKey
-pr = cProfile.Profile()
-pr.enable()
-
-
-with open("input.txt") as f:
+with open("input.txt") as f:         #4014 function calls in 0.107 seconds
     for line in f:
         line = line.strip()[::-1]
         print(line)
-
-        
-pr.disable()
-s = io.StringIO()
-sortby = SortKey.CUMULATIVE
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print(s.getvalue())
