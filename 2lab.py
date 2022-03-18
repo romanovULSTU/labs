91f5b1769bdaa3dbb2da790c8add5490a98ccfeb
romanovULSTU <99944605+romanovULSTU@users.noreply.github.com>
2022-03-18 23:02:14 +0400

    Update 2lab.py

--- 2lab.py
+++ 2lab.py
@@ -1,21 +1,37 @@
-import cProfile, pstats  ,io    #4052 function calls in 0.091 seconds
-from pstats import SortKey               
-pr = cProfile.Profile()                         
-pr.enable()
+import os
+import time
 
- 
-data = open ("input.txt",'r')   #открываем файл                 
-while True:
-    line = data.readline()                                                                                 
-    if not line:                #выходим из цикла , если строка пустая     
-        break                    
-    print(line.strip()[::-1])   #выводим переворот срезом
+more_max_buffer_len = False  
+max_buffer_len = 200    # максимальный размер рабочего буфера
 
+buffer_len = 1          # размер буфера чтения
+work_buffer = ""        # рабочий буфер
+
+
+try:
+    print("\nxXx--Результат работы программы--xXx\nxXx--Локальное время",time.ctime(),"--xXx\n")
+    start = time.time()       
+    with open("input.txt", "r") as file:   # открываем файл
+        line = file.read(buffer_len)          
+        if not line:                        # если файл пустой
+            print ("\nФайл input.txt в директории проекта пустой.\nДобавьте не пустой файл в директорию или переименуйте существующий *.txt файл.")
+        for line in file:                   # пока есть строки в файле    
+            if line:   
+                work_buffer += '*'
+                if len(work_buffer) >= max_buffer_len :    # Если буфер переполнен
+                    print ("\nФайл input.txt содержит блок цифр, превышающий максимальный размер буфера = "+str(max_buffer_len) + " строк.\nОткорректируйте файл text.txt в директории или переименуйте существующий *.txt файл.")
+                    more_max_buffer_len = True
+                    if more_max_buffer_len:
+                        break
+                else:
+                    line = line.strip()[::-1]       #переворачиваем числа
+                    print(line)
+            
+        finish = time.time()
+        result = finish - start
+        print("Program time: " + str(result) + " seconds.")
+        
+except FileNotFoundError:
+    print("\nФайл input.txt в директории проекта не обнаружен.\nДобавьте файл в директорию или переименуйте существующий *.txt файл.") 
     
-pr.disable()
-s = io.StringIO()
-sortby = SortKey.CUMULATIVE
-ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-ps.print_stats()
-print(s.getvalue())
 
